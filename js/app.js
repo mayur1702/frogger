76f1ab4eb7a3409e5bd92670d51b83364b98a712
unknown <m.mistry17@yahoo.com>
2017-09-12 19:28:13 +0530

    missing semicolons

--- js/app.js
+++ js/app.js
@@ -15,7 +15,11 @@ var Bonus=function(x,y){
     this.y=y;
     this.sprite='images/Star.png';
 
-}
+};
+
+var character=function(a,b,c){
+    ctx.drawImage(a,b,c);
+};
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
@@ -29,66 +33,70 @@ Enemy.prototype.update = function(dt) {
 };
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+   character(Resources.get(this.sprite), this.x, this.y);
 };
 // Draw the bonus objects on screen
 Bonus.prototype.render=function(f){
     if(f!=0)
     {
-       ctx.drawImage(Resources.get(this.sprite), this.x,this.y);
+       character(Resources.get(this.sprite), this.x, this.y);
    }
-}
+};
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function(x,y){
+var Player = function(x,y,forward){
     this.x=x;
     this.y=y;
     this.sprite="images/char-boy.png";
 };
 Player.prototype.update=function(){
-$("#boy").click(function(){
-     player.sprite=  "images/char-boy.png"});
-$("#girl").click(function(){
-     player.sprite=  "images/char-pink-girl.png"});
-$("#horn").click(function(){
-     player.sprite=  "images/char-horn-girl.png"});
-$("#cat").click(function(){
-     player.sprite=  "images/char-cat-girl.png"});
-$("#princess").click(function(){
-     player.sprite=  "images/char-princess-girl.png"});
+switch( temp){
+    case 1:this.sprite="images/char-boy.png";
+            break;
+    case 2:this.sprite="images/char-pink-girl.png";
+            break;
+    case 3:this.sprite="images/char-horn-girl.png";
+            break;
+    case 4:this.sprite="images/char-cat-girl.png";
+            break;
+    case 5:this.sprite="images/char-princess-girl.png";
+            break;        
+}
 };
 Player.prototype.render=function(){
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    character(Resources.get(this.sprite), this.x, this.y);
  };
  Player.prototype.handleInput=function(keyPress){
-    if (keyPress == 'left'&& player.x>=100) {
-        player.x -= 100;
+    if (keyPress == 'left'&& this.x>=80) {
+        this.x -= 101;
     }
-    if (keyPress == 'up'&& player.y>10) {
-        player.y -= 82;
+    if (keyPress == 'up'&& this.y>10) {
+        this.y -= 83;
     }
-    if (keyPress == 'right'&& player.x<400) {
-        player.x += 100;
+    if (keyPress == 'right'&& this.x<400) {
+        this.x += 101;
     }
-    if (keyPress == 'down'&& player.y<=320) {
-        player.y += 82;
+    if (keyPress == 'down'&& this.y<=320) {
+        this.y += 83;
     }
- }
+ };
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+//var character=new Character();
+var temp;
 var allEnemies=[];
 var score=0;
 var possible=[65,140,220];
-var playerPossibleX=[0,100,200,300,400];
-var playerPossibleY=[320,400];
-var player=new Player(playerPossibleX[Math.floor(Math.random() * playerPossibleX.length)],playerPossibleY[Math.floor(Math.random() * playerPossibleY.length)]);
+var player_x=[0,100,200,300,400];
+var player_y=[320,400];
+var player=new Player(player_x[Math.floor(Math.random() * player_x.length)],player_y[Math.floor(Math.random() * player_y.length)]);
 var lives=3;
 var level=1;
 var j=1;
 var enemy = new Enemy(0,possible[Math.floor(Math.random() * possible.length)],Math.random() * 256);
-var bonus= new Bonus(playerPossibleX[Math.floor(Math.random() * playerPossibleX.length)],possible[Math.floor(Math.random() * possible.length)]);
+var bonus= new Bonus(player_x[Math.floor(Math.random() * player_x.length)],possible[Math.floor(Math.random() * possible.length)]);
 
 var reset=function(){
     lives=3;
@@ -96,13 +104,13 @@ var reset=function(){
     level=1;
     nextLevel(0);
     stats(0,3);
-}
+};
 // Displaying stats
 var stats=function(score,lives){
     $("#scores").text("Your Score :"+score);
     $("#lives").text("Lives remaining :" +lives);
     $("#level").text("Level: "+level);
-}
+};
 // Initial stats
 stats(0,3);
 // checking collision or level up
@@ -111,16 +119,16 @@ var checkcol=function(ene){
         alert("Collision!!!!!");
         lives--;
             stats(score,lives);    
-        player.x=playerPossibleX[Math.floor(Math.random() * playerPossibleX.length)];
-        player.y=playerPossibleY[Math.floor(Math.random() * playerPossibleY.length)];
+        player.x=player_x[Math.floor(Math.random() * player_x.length)];
+        player.y=player_y[Math.floor(Math.random() * player_y.length)];
 }
     if(player.y<65){
         level+=1;
         console.log(score);
         nextLevel(level);
         stats(score,lives);
-        player.x=playerPossibleX[Math.floor(Math.random() * playerPossibleX.length)];
-        player.y=playerPossibleY[Math.floor(Math.random() * playerPossibleY.length)];
+        player.x=player_x[Math.floor(Math.random() * player_x.length)];
+        player.y=player_y[Math.floor(Math.random() * player_y.length)];
     }
     if(lives===0){
           reset();    
@@ -128,7 +136,7 @@ var checkcol=function(ene){
     if(j!==0)
     checkpos();    
 
-}
+};
 
 var checkpos=function(){
      if(Math.abs(player.x-bonus.x)<80 && Math.abs(player.y-bonus.y)<50 && lives>0){
@@ -137,7 +145,7 @@ var checkpos=function(){
        f=0;
        stats(score,lives);    
     }
-}
+};
 allEnemies.push(enemy);
 // increase difficulty
 var nextLevel = function(strength) {
@@ -145,7 +153,7 @@ var nextLevel = function(strength) {
     allEnemies.length = 0;
     f=1;
     j=1;
-    bonus.x=playerPossibleX[Math.floor(Math.random() * playerPossibleX.length)];
+    bonus.x=player_x[Math.floor(Math.random() * player_x.length)];
         bonus.y=possible[Math.floor(Math.random() * possible.length)];
     // load new set of enemies
     for (var i = 0; i <= strength; i++) {
@@ -164,3 +172,20 @@ document.addEventListener('keyup', function(e) {
     };
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+
+document.getElementById('boy').onclick=function(){
+    temp=1;
+   };
+document.getElementById('girl').onclick=function(){
+    temp=2;
+   };
+document.getElementById('horn').onclick=function(){
+    temp=3;
+   };
+document.getElementById('cat').onclick=function(){
+    temp=4;
+   };
+document.getElementById('princess').onclick=function(){
+    temp=5;
+   };      
